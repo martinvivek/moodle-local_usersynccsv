f13e0b9b62330518a5d874a455b2eef47f4098fc
tunele <antonello.moro1@gmail.com>
2016-06-30 20:32:02 +0200

    travis test

--- settings.php
+++ settings.php
@@ -19,7 +19,7 @@ defined('MOODLE_INTERNAL') || die();
 $context = context_system::instance();
 
 if ($hassiteconfig) {
-  $settings = new admin_settingpage('local_usersynccsv', get_string('pluginname', 'local_usersynccsv'));
-  $settings->add(new admin_setting_configselect('local_usersynccsv/importdir',
-      get_string('importdir', 'local_usersynccsv'), get_string('importdirdesc', 'local_usersynccsv'),''));
+    $settings = new admin_settingpage('local_usersynccsv', get_string('pluginname', 'local_usersynccsv'));
+    $settings->add(new admin_setting_configselect('local_usersynccsv/importdir',
+        get_string('importdir', 'local_usersynccsv'), get_string('importdirdesc', 'local_usersynccsv'), ''));
 }
