e87af6e76c3ea9bec24db4b61f70a60af0b41fc6
tunele <antonello.moro1@gmail.com>
2016-06-30 21:00:42 +0200

    settings

--- settings.php
+++ settings.php
@@ -20,6 +20,6 @@ $context = context_system::instance();
 
 if ($hassiteconfig) {
     $settings = new admin_settingpage('local_usersynccsv', get_string('pluginname', 'local_usersynccsv'));
-    $settings->add(new admin_setting_configselect('local_usersynccsv/importdir',
+    $settings->add(new admin_setting_configtext('local_usersynccsv/importdir',
         get_string('importdir', 'local_usersynccsv'), get_string('importdirdesc', 'local_usersynccsv'), ''));
 }
