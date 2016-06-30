ede73d227b1a5fd01d0495564e09d1e9eb917232
tunele <antonello.moro1@gmail.com>
2016-06-30 20:20:33 +0200

    travis test

---
+++ lang/en/local_usersynccsv.php
@@ -0,0 +1,8 @@
+<?php
+$string['pluginname'] = 'User Sync CSV';
+$string['modulenameplural'] = 'User Sync CSV';
+$string['modulename_help'] = 'Synchronize Users with external system by means of CSV files';
+//$string['usersynccsv'] = 'batchcompletamenti';
+$string['pluginadministration'] = 'User Sync CSV administration';
+$string['importdir'] = 'Import directory';
+$string['importdirdesc'] = 'Full path of the working directory. Must have read/write access';
---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2016070101;      // The current module version (Date: YYYYMMDDXX).
+$plugin->requires  = 2014050800;      // Requires this Moodle version.
+$plugin->component = 'local_usersynccsv';// Full name of the plugin (used for diagnostics).
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0.0';
