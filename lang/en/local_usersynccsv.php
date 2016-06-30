21112dc595578f344420be945493fc170f45e35f
tunele <antonello.moro1@gmail.com>
2016-06-30 20:25:42 +0200

    travis test

--- lang/en/local_usersynccsv.php
+++ lang/en/local_usersynccsv.php
@@ -1,8 +1,23 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 $string['pluginname'] = 'User Sync CSV';
 $string['modulenameplural'] = 'User Sync CSV';
 $string['modulename_help'] = 'Synchronize Users with external system by means of CSV files';
-//$string['usersynccsv'] = 'batchcompletamenti';
+
 $string['pluginadministration'] = 'User Sync CSV administration';
 $string['importdir'] = 'Import directory';
 $string['importdirdesc'] = 'Full path of the working directory. Must have read/write access';
